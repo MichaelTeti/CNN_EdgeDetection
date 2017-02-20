a5c6c664be17d92b166ab0e0803d0b8e5e89f84f
michael teti <mtetifau@gmail.com>
2017-02-20 10:08:27 -0500

    cleaned up a little

--- EdgeCNN.m
+++ EdgeCNN.m
@@ -22,15 +22,14 @@ a=reshape(gene(11:end), 3, 3);
 %im=im(1:3:end, 1:3:end);
 im=[im(:, 1) im im(:, end)];
 im=[im(1, :); im; im(end, :)];
-init_im=im;
-init_im(:, :)=0;
+init_im=zeros(size(im))
 prod=zeros(size(im));
 iter=0;
 while iter < 1
     for i=2:size(im, 1)-1;
         for j=2:size(im, 2)-1;
             influence=im(i-1:i+1, j-1:j+1);
-            prod(i, j)=init_im(i, j)+z+(sum(sum(influence.*b)));
+            prod(i, j)=init_im(i, j)+z+influence'*b;
             out=(exp(prod(i, j))-exp(-prod(i, j)))/(exp(prod(i, j))+exp(-prod(i, j)));
             prod(i, j)=prod(i, j)+out*a(2, 2);
             init_im(i, j)=prod(i, j);
