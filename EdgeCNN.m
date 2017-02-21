52d15b4b15927f6f6dd651f0f3b426d3dd0f9bed
michael teti <mtetifau@gmail.com>
2017-02-21 12:29:37 -0500

    put in convolutions

--- EdgeCNN.m
+++ EdgeCNN.m
@@ -13,33 +13,41 @@ close all;
 %load image
 
 im=imread('image2.jpg');
-im=im2double(im);
-im=rgb2gray(im);
-gene=[-.5 -1 -1 -1 -1 8 -1 -1 -1 -1 0 0 0 0 2 0 0 0 0];
-z=gene(1);
-b=reshape(gene(2:10), 3, 3);
-a=reshape(gene(11:end), 3, 3);
-%im=im(1:3:end, 1:3:end);
-im=[im(:, 1) im im(:, end)];
-im=[im(1, :); im; im(end, :)];
-init_im=zeros(size(im))
-prod=zeros(size(im));
-iter=0;
-while iter < 1
-    for i=2:size(im, 1)-1;
-        for j=2:size(im, 2)-1;
-            influence=im(i-1:i+1, j-1:j+1);
-            prod(i, j)=init_im(i, j)+z+influence'*b;
-            out=(exp(prod(i, j))-exp(-prod(i, j)))/(exp(prod(i, j))+exp(-prod(i, j)));
-            prod(i, j)=prod(i, j)+out*a(2, 2);
-            init_im(i, j)=prod(i, j);
-        end
-    end 
-    iter=iter+1;
-end 
 
-subplot(1, 2, 1);
-imshow(im)
-subplot(1, 2, 2);
-imshow(prod)
+im=rgb2gray(im2double(im));
+
+gene=[-.5 -1 -1 -1 -1 8 -1 -1 -1 -1 0 0 0 0 2 0 0 0 0];
+
+z=gene(1);
+
+b=reshape(gene(2:10), 3, 3);
+
+a=reshape(gene(11:end), 3, 3);
+
+init_im=0.*im;
+
+dt=0.1
+
+B0=conv2(init_im, b, 'same')
+
+for iters=1:100
+
+  dx=-im+conv2(f(im), a, 'same')+B0+Z;
+
+  x=x+dx.*dt;
+
+  
+  subplot(1, 2, 1);
+  imshow(im)
+  subplot(1, 2, 2);
+  imshow(prod)
+
+end;
         
+
+
+
+
+function y=thresh(x)
+
+y=(abs(x+1)/2 - abs(x-1)/2);
