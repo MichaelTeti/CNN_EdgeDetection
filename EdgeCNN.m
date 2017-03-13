020218d304cc3d450740c0e76de16cda567b805f
Michael A. Teti <mtetifau@gmail.com>
2017-03-13 13:00:36 -0400

    cleaned up a little

--- EdgeCNN.m
+++ EdgeCNN.m
@@ -16,6 +16,8 @@ im=imread('image2.jpg');
 
 im=rgb2gray(im2double(im));
 
+x=im;
+
 gene=[-.5 -1 -1 -1 -1 8 -1 -1 -1 -1 0 0 0 0 2 0 0 0 0];
 
 z=gene(1);
@@ -24,7 +26,7 @@ b=reshape(gene(2:10), 3, 3);
 
 a=reshape(gene(11:end), 3, 3);
 
-init_im=0.*im;
+init_im=0.*x;
 
 dt=0.1
 
@@ -32,7 +34,7 @@ B0=conv2(init_im, b, 'same')
 
 for iters=1:100
 
-  dx=-im+conv2(f(im), a, 'same')+B0+Z;
+  dx=-im+conv2(f(x), a, 'same')+B0+Z;
 
   x=x+dx.*dt;
 
@@ -40,7 +42,7 @@ for iters=1:100
   subplot(1, 2, 1);
   imshow(im)
   subplot(1, 2, 2);
-  imshow(prod)
+  imshow(x)
 
 end;
         
